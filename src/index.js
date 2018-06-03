eac07766e8d22f2091d0ce32b8950e0c58cfe9af
fjc0k <fjc0kb@gmail.com>
2018-06-03 20:23:24 +0800

    feat: refactor core

--- src/index.js
+++ src/index.js
@@ -1,31 +1,16 @@
-const path = require('path')
-const SVGO = require('svgo')
-const defaultSVGOOptions = require('./defaultSVGOOptions')
+const convertSVGToSymbol = require('./convertSVGToSymbol')
 
 module.exports = function (SVGContent) {
   this.cacheable && this.cacheable()
 
   const done = this.async()
 
-  const {
-    extractId = ({ name }) => name,
-    svgo: SVGOptions = defaultSVGOOptions
-  } = this.query || {}
-
-  const svgo = new SVGO({ plugins: [SVGOptions] })
-
-  const { name } = path.parse(this.resourcePath)
-
-  const id = typeof extractId === 'function' ? extractId.call(this, { name }) : name
-
-  svgo.optimize(SVGContent).then(({ data }) => {
-    data = data.trim().replace(
-      /^<svg\s*([^>]*)>(.*)<\/svg>$/i,
-      (_, attrs, content) => {
-        attrs = attrs.replace(/id=\S+/i, '')
-        return `<symbol id="${id}"${attrs ? ' ' + attrs : ''}>${content}</symbol>`
-      }
-    )
-    done(null, `module.exports = ${JSON.stringify(data)}`)
-  })
+  convertSVGToSymbol(
+    this.resourcePath,
+    SVGContent,
+    this.query || {}
+  ).then(
+    data => done(null, `module.exports = ${JSON.stringify(data)}`),
+    err => done(err)
+  )
 }
